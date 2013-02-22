f618b6146532124fc2603a3f6869fe2a9f96faab
Simon Godden <simon@blackcatsolutions.co.uk>
2013-02-22 16:18:44 +0000

    Basic skeleton showing a combo button

--- MultiSelectComboBox.js
+++ MultiSelectComboBox.js
@@ -1,1 +1,16 @@
-define([], function(){});
+define([
+    "dojo/_base/declare",
+    "dgrid/OnDemandGrid",
+    "dijit/_Widget",
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetsInTemplateMixin",
+    "dojo/text!./resources/MultiSelectComboBox.html",
+
+    "dijit/form/ComboButton"
+], function (declare, OnDemandGrid, _Widget, _TemplatedMixin, _WidgetsInTemplatedMixin, template) {
+
+    return declare("dgrid-multiselect-combo.MultiSelectComboBox", [_Widget, _TemplatedMixin, _WidgetsInTemplatedMixin], {
+        templateString: template
+    });
+
+});
