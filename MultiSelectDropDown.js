ed978373593fc2002971c95f4d5ac928553caacf
Simon Godden <simon@blackcatsolutions.co.uk>
2013-02-24 08:50:38 +0000

    Some progress towards using a dgrid.

--- MultiSelectDropDown.js
+++ MultiSelectDropDown.js
@@ -5,12 +5,34 @@ define([
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     "dojo/text!./resources/MultiSelectDropDown.html",
+    "dojo/store/Memory",
 
     "dijit/form/TextBox"
-], function (declare, OnDemandGrid, _Widget, _TemplatedMixin, _WidgetsInTemplatedMixin, template) {
+], function (declare, OnDemandGrid, _Widget, _TemplatedMixin, _WidgetsInTemplatedMixin, template, Memory) {
+
+    var MyGrid = declare([OnDemandGrid], {
+        columns: {
+            "foo": {label: "Foo"}
+        }
+    });
 
     return declare("dgrid-multiselect-combo.MultiSelectDropDown", [_Widget, _TemplatedMixin, _WidgetsInTemplatedMixin], {
-        templateString: template
+        templateString: template,
+
+        dapGrid: null,
+
+        postCreate: function() {
+            var grid = new MyGrid({
+                store: new Memory({
+                    data: [
+                        {foo: 'bar'}
+                    ]
+                })
+            });
+            this.dapGrid.appendChild(grid.domNode);
+            grid.startup();
+            grid.resize();
+        }
     });
 
 });
